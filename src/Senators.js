f15b7581223051a8b9b75b9c697a88c4bab96841
Manny <coachmannytw@gmail.com>
2018-02-26 15:34:57 -0700

    updated

--- src/Senators.js
+++ src/Senators.js
@@ -2,40 +2,51 @@ import React, { Component } from 'react'
 import Senators from './data/senators'
 
 export default class extends Component {
-  //
-  // state = {
-  //   name: '',
-  //   party: [],
-  //   senators: []
-  // }
 
-  filterSenatorsByName(name) {
-    return Senators.filter((senator) => {
-      
+  state = {
+    name: '',
+    party: [],
+    reps: []
+  }
 
-    })
+
+
+
+
+  filterSenatorsByName() {
+
+      return Senators.filter( (human) => { return human.person.name.match(this.state.name) } )
+
   }
-  
-  renderSenators() {
-    return Senators.map(senator => (
-      <div>{senator.person.firstname} {senator.person.lastname}</div>
-      // <div> {senator.person.lastname}
-    ))
-  }
-  
+
+
+    renderSenators() {
+
+
+        return this.filterSenatorsByName().map(senator => (
+            <div>{senator.person.firstname} {senator.person.lastname} {senator.state} {senator.phone} {senator.party} {senator.website}</div>
+
+
+        ))
+    }
+
+
   render() {
     return (
       <div>
         <h1>Senators</h1>
         <form>
           <div>
-            {/*<input type="search" placeholder="Filter by Name" value={this.state.name} onChange={e => this.setState({ name: e.target.value })} />*/}
+            <input type="search" placeholder="Filter by Name" value={this.state.name} onChange={e => this.setState({ name: e.target.value })} />
           </div>
           <div>
             <input type="text" />
           </div>
         </form>
+        <div>
         {this.renderSenators()}
+        </div>
+
       </div>
     )
   }
